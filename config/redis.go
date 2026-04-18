9ad26de1431129e0223f6648da5aac4df4252425
aamir-sufiyan-n <aamirsufiyan848@gmail.com>
2026-04-18 11:52:30 +0530

    finished

--- config/redis.go
+++ config/redis.go
@@ -1,19 +1,19 @@
 package config
 
 import (
-    "context"
-    "os"
+	"context"
+	"os"
 
-    "github.com/redis/go-redis/v9"
+	"github.com/redis/go-redis/v9"
 )
 
 var Ctx = context.Background()
 
 func InitRedis() *redis.Client {
-    rdb := redis.NewClient(&redis.Options{
-        Addr:     os.Getenv("REDIS_ADDR"),     
-        Password: os.Getenv("REDIS_PASSWORD"), 
-        DB:       0,
-    })
-    return rdb
+	opt, err := redis.ParseURL(os.Getenv("REDIS_URL"))
+	if err != nil {
+		panic(err)
+	}
+	rdb := redis.NewClient(opt)
+	return rdb
 }
